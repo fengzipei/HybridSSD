e1a8c3dba5936a40f99d5b892ed19eb6c9b83ae8
fengzipei <fengzipei3@gmail.com>
2017-04-27 21:42:20 +0800

    init

---
+++ cmake-build-debug/CMakeFiles/HybridSSD.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HybridSSD.dir/avlTree.c.o"
+  "CMakeFiles/HybridSSD.dir/flash.c.o"
+  "CMakeFiles/HybridSSD.dir/initialize.c.o"
+  "CMakeFiles/HybridSSD.dir/pagemap.c.o"
+  "CMakeFiles/HybridSSD.dir/ssd.c.o"
+  "HybridSSD.pdb"
+  "HybridSSD.exe"
+  "libHybridSSD.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/HybridSSD.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
